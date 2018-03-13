e5933e95213646d47dd66b0f70d526652b24f19e
Trevor Seitz <trevinla@gmail.com>
2018-03-13 20:11:42 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,11 +3,15 @@ require 'pry'
 def my_all?(collection)
   binding.pry
   i = 0
-block_return_values = []
-while i < collection.length
-  block_return_values << yield(collection[i])
-  yield(collection[i])
-  i += 1
-end
-collection
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    yield(collection[i])
+    i += 1
+  end
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
